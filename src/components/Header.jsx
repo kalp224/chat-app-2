753b1cd2fc05bb01a21a4aa0ed554d95ae3435a7
KALP PRAJAPATI <99316165+kalp224@users.noreply.github.com>
2023-10-08 00:09:40 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Flex, Avatar, AvatarBadge, Text } from "@chakra-ui/react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <Flex w="100%">
-      <Avatar size="lg" name="Dan Abrahmov" src="https://bit.ly/dan-abramov">
-        <AvatarBadge boxSize="1.25em" bg="green.500" />
-      </Avatar>
-      <Flex flexDirection="column" mx="5" justify="center">
-        <Text fontSize="lg" fontWeight="bold">
-          Ferin Patel
-        </Text>
-        <Text color="green.500">Online</Text>
+    <Flex w="100%" justify="space-between" align="center">
+      <Flex align="center">
+        <Avatar size="lg" name="Dan Abrahmov" src="https://bit.ly/dan-abramov">
+          <AvatarBadge boxSize="1em" bg="green.500" />
+        </Avatar>
+        <Flex flexDirection="column" mx="5" justify="center">
+          <Text fontSize="lg" fontWeight="bold">
+            Ferin Patel
+          </Text>
+          <Text color="green.500">Online</Text>
+        </Flex>
       </Flex>
+      <MoreVertIcon style={{ marginLeft: "auto", marginRight: "20px" }} />
     </Flex>
   );
 };
